538261d4b82a619a68bc150bebe7ee2e18c4e8f3
ukmjkim <ukmjkim@gmail.com>
2020-01-25 17:50:52 -0800

    initial upload

---
+++ spring/src/test/java/com/bizrun/appmain/AppMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizrun.appmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
